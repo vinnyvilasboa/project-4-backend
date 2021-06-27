c50907d7f64525fc164ab39ff7a169c3a98594f1
Jordan Wright <81875454+behemoth0132@users.noreply.github.com>
2021-06-27 01:49:15 -0700

    added 1- 25 movies to seed

--- api/favorites.js
+++ api/favorites.js
@@ -6,50 +6,50 @@ const passport = require('passport');
 
 
 // Models
-const { Book } = require('../models');
+const { favorites } = require('../models');
 
 // Controllers
-const index = async (req, res) => {
-    console.log('inside of /api/books');
+const index = async(req, res) => {
+    console.log('inside of /api/favorites');
     try {
-        const allBooks = await Book.find({});
+        const allfavorites = await favorites.find({});
 
-        res.json({ books: allBooks });
+        res.json({ favorites: allfavorites });
     } catch (error) {
-        console.log('Error inside of /api/books');
+        console.log('Error inside of /api/favorites');
         console.log(error);
         return res.status(400).json({ message: 'Books not found. Please try again.' });
     }
 }
 
-const show = async (req, res) => {
+const show = async(req, res) => {
     const { id } = req.params;
     try {
         // look for book based on id
-        const book = await Book.findById(id);
-        res.json({ book });
+        const favorites = await favorites.findById(id);
+        res.json({ favorites });
     } catch (error) {
-        console.log('Error inside of /api/books/:id');
+        console.log('Error inside of /api/favorites/:id');
         console.log(error);
         return res.status(400).json({ message: 'Book not found. Try again...' });
     }
 }
 
-const create = async (req, res) => {
+const create = async(req, res) => {
     const { title, author, price, pages, isbn, genre } = req.body;
 
     try {
-        const newBook = await Book.create({ title, author, price, pages, isbn, genre });
-        console.log('new book created', newBook);
-        res.json({ book: newBook });
+        const newfavorites = await favorites.create({ title, author, price, pages, isbn, genre });
+        console.log('new favorite created', newfavorites);
+        res.json({ favorites: newfavorites });
     } catch (error) {
-       console.log('Error inside of POST of /api/books');
-       console.log(error);
-       return res.status(400).json({ message: 'Book was not created. Please try again...' }); 
+        console.log('Error inside of POST of /api/books');
+        console.log(error);
+        return res.status(400).json({ message: 'Book was not created. Please try again...' });
     }
 }
 
-const update = async (req, res) => {
+const update = async(req, res) => {
     console.log(req.body);
     try {
         // const book = await Book.findOne({ title: req.body.title });
@@ -64,13 +64,13 @@ const update = async (req, res) => {
         // // save the new book info
         // const savedBook = await book.save();
 
-        const updatedBook = await Book.update({ title: req.body.title }, req.body); // updating the book
-        const book = await Book.findOne({ title: req.body.title });
+        const updatedfavorites = await favorites.update({ title: req.body.title }, req.body); // updating the book
+        const favorites = await favorites.findOne({ title: req.body.title });
 
-        console.log(updatedBook); // { n: 1, nModified: 0, ok: 1 }
-        console.log(book); // a book object 
+        console.log(updatedfavorites); // { n: 1, nModified: 0, ok: 1 }
+        console.log(favorites); // a book object 
 
-        res.redirect(`/api/books/${book.id}`);
+        res.redirect(`/api/favorites/${favorites.id}`);
 
     } catch (error) {
         console.log('Error inside of UPDATE route');
@@ -79,13 +79,13 @@ const update = async (req, res) => {
     }
 }
 
-const deleteBook = async (req, res) => {
+const deleteFavorite = async(req, res) => {
     const { id } = req.params;
     try {
         console.log(id);
-        const result = await Book.findByIdAndRemove(id);
+        const result = await favorites.findByIdAndRemove(id);
         console.log(result);
-        res.redirect('/api/books');
+        res.redirect('/api/favorites');
     } catch (error) {
         console.log('inside of DELETE route');
         console.log(error);
@@ -95,11 +95,11 @@ const deleteBook = async (req, res) => {
 
 // GET api/books/test (Public)
 router.get('/test', (req, res) => {
-    res.json({ msg: 'Books endpoint OK!'});
+    res.json({ msg: 'favorites endpoint OK!' });
 });
 
 // GET -> /api/books/
-router.get('/', passport.authenticate('jwt', { session: false }), index); 
+router.get('/', passport.authenticate('jwt', { session: false }), index);
 // GET -> /api/books/:id
 router.get('/:id', passport.authenticate('jwt', { session: false }), show);
 // POST -> /api/books
@@ -107,6 +107,6 @@ router.post('/', passport.authenticate('jwt', { session: false }), create);
 // PUT -> /api/books
 router.put('/', passport.authenticate('jwt', { session: false }), update);
 // DELETE => /api/books/:id
-router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteBook);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), deletefavorites);
 
 module.exports = router;
